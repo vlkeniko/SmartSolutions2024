bbd402550de9bc89319e43a47ed05f6abe3819bc
Enikő Valkó <eaaenva@students.eaaa.dk>
2024-03-21 10:48:33 +0100

    Switching to Visual Studio :snowboarder:

--- VerySmartSolutions/Assets/Scenes/EnikoScript.cs
+++ VerySmartSolutions/Assets/Scenes/EnikoScript.cs
@@ -7,7 +7,7 @@ public class EnikoScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+    
     }
 
     // Update is called once per frame
